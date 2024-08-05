c085b28a95c80892da5341b9b1e3780510054e0b
AbhishekDefra <abhishek.singha@defra.gov.uk>
2024-08-05 13:47:28 +0100

    error pages changes

--- src/server/common/helpers/errors.js
+++ src/server/common/helpers/errors.js
@@ -1,37 +1,38 @@
 function statusCodeMessage(statusCode) {
   switch (true) {
     case statusCode === 404:
-      return 'Page not found'
-    case statusCode === 403:
-      return 'Forbidden'
-    case statusCode === 401:
-      return 'Unauthorized'
-    case statusCode === 400:
-      return 'Bad Request'
+      return 'If you typed the web address, check it is correct. <br><br> If you pasted the web address, check you copied the entire address.'
     default:
-      return 'Something went wrong'
+      return 'Try again later.<br><br>Email <a href="mailto:disinfectant@apha.gov.uk"> disinfectant@apha.gov.uk </a> for further information.'
   }
 }
-
+function statusCodeHeading(statusCode) {
+  switch (true) {
+    case statusCode === 404:
+      return {
+        title: 'Page Not Found'
+      }
+    default:
+      return {
+        title: 'Sorry, there is a problem with the service'
+      }
+  }
+}
 function catchAll(request, h) {
   const { response } = request
-
   if (!response.isBoom) {
     return h.continue
   }
-
   request.logger.error(response?.stack)
-
   const statusCode = response.output.statusCode
   const errorMessage = statusCodeMessage(statusCode)
-
+  const errorHeading = statusCodeHeading(statusCode)
   return h
     .view('error/index', {
-      pageTitle: errorMessage,
-      heading: statusCode,
+      pageTitle: errorHeading.title,
+      heading: errorHeading.title,
       message: errorMessage
     })
     .code(statusCode)
 }
-
 export { catchAll }
