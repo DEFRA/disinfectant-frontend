081596e9206b61fd51c1e99b34ad1bbdb28fb1c5
praveenrajdefra <Praveen.Rajendran@defra.gov.uk>
2024-09-15 22:28:08 +0100

    updated the code to fix sonar could refactor issue for the fetch data method..

--- src/server/approved-disinfectants/helpers/fetch-data.js
+++ src/server/approved-disinfectants/helpers/fetch-data.js
@@ -10,6 +10,7 @@ const options = { method: 'GET', headers: { 'Content-Type': 'text/json' } }
 const logger = createLogger()
 const disInfectant = config.get('disinfectant')
 const rediskey = 'disinfectantApprovedData'
+
 const fetchData = async (
   request,
   chemicalGroupSelected,
@@ -27,44 +28,75 @@ const fetchData = async (
     getApprovedListResponse = await request.redis.getData(rediskey)
   }
 
-  let response
   if (!getApprovedListResponse) {
-    logger.info(
-      `fetch-data method - No Data in Cache. API endpoint ${disInfectant.apiPath}${appSpecificConstants.apiEndpoint.retrieveList} Invocation started`
-    )
-    response = await proxyFetch(
-      `${disInfectant.apiPath}${appSpecificConstants.apiEndpoint.retrieveList}`,
-      options
-    ).catch((err) => {
-      logger.info(
-        `error while calling api endpoint ${JSON.stringify(err.message)}`
-      )
-      throw err
-    })
-    try {
-      if (response.ok) {
-        getApprovedListResponse = await response.json()
-        if (redisConfig.enabled) {
-          request.redis.storeData(rediskey, getApprovedListResponse)
-        }
-      }
-    } catch (err) {
-      logger.info(`error while getting data from api response : ${err}`)
-      throw err
-    }
+    getApprovedListResponse = await fetchApprovedListFromAPI(request)
   }
 
+  const {
+    approvedDisinfectantList,
+    checmicalGroups,
+    lastModifiedDateWithTime,
+    lastModifiedDate
+  } = processApprovedList(
+    getApprovedListResponse,
+    chemicalGroupSelected,
+    approvalCategoriesSelected,
+    searchText,
+    startsWith
+  )
+
+  return {
+    approvedDisinfectantList,
+    checmicalGroups,
+    lastModifiedDateWithTime,
+    lastModifiedDate
+  }
+}
+
+const fetchApprovedListFromAPI = async (request) => {
+  logger.info(
+    `fetch-data method - No Data in Cache. API endpoint ${disInfectant.apiPath}${appSpecificConstants.apiEndpoint.retrieveList} Invocation started`
+  )
+  const response = await proxyFetch(
+    `${disInfectant.apiPath}${appSpecificConstants.apiEndpoint.retrieveList}`,
+    options
+  ).catch((err) => {
+    logger.info(
+      `error while calling api endpoint ${JSON.stringify(err.message)}`
+    )
+    throw err
+  })
+
+  try {
+    if (response.ok) {
+      const getApprovedListResponse = await response.json()
+      if (redisConfig.enabled) {
+        request.redis.storeData(rediskey, getApprovedListResponse)
+      }
+      return getApprovedListResponse
+    }
+  } catch (err) {
+    logger.info(`error while getting data from api response : ${err}`)
+    throw err
+  }
+}
+
+const processApprovedList = (
+  getApprovedListResponse,
+  chemicalGroupSelected,
+  approvalCategoriesSelected,
+  searchText,
+  startsWith
+) => {
   let approvedDisinfectantList = []
   let checmicalGroups = []
   let lastModifiedTime = null
   let lastModifiedDateWithTime = null
   let lastModifiedDate = null
+
   try {
-    checmicalGroups = getApprovedListResponse?.documents[0]?.chemicalGroups
-      ? getApprovedListResponse.documents[0].chemicalGroups
-      : []
-    lastModifiedTime =
-      getApprovedListResponse?.documents[0]?.lastModifiedDateAndTime
+    checmicalGroups = getChemicalGroups(getApprovedListResponse)
+    lastModifiedTime = getLastModifiedTime(getApprovedListResponse)
 
     if (typeof lastModifiedTime !== 'undefined' && lastModifiedTime !== null) {
       lastModifiedDateWithTime = formatDate(
@@ -73,162 +105,46 @@ const fetchData = async (
       )
       lastModifiedDate = formatDate(lastModifiedTime, 'd MMMM yyyy')
     }
-    approvedDisinfectantList = getApprovedListResponse?.documents[0]
-      ?.disInfectants
-      ? getApprovedListResponse.documents[0].disInfectants
-      : []
-    logger.info(
-      `approved list disinfectants ${JSON.stringify(approvedDisinfectantList)}`
-    )
+
+    approvedDisinfectantList = getDisinfectantList(getApprovedListResponse)
+
     if (approvedDisinfectantList.length < 1) {
       logger.info(`approved list disinfectants less than 1 record`)
       throw new Error('no records fetched')
     }
-    approvedDisinfectantList = approvedDisinfectantList.sort(function (a, b) {
-      return a.disInfectantName.localeCompare(b.disInfectantName)
-    })
-    // chemical group filtration
-    if (chemicalGroupSelected) {
-      if (Array.isArray(chemicalGroupSelected)) {
-        if (chemicalGroupSelected.length > 0) {
-          chemicalGroupSelected = chemicalGroupSelected.reduce(
-            (unique, item) =>
-              unique.includes(item) ? unique : [...unique, item],
-            []
-          )
 
-          approvedDisinfectantList = approvedDisinfectantList.filter(
-            (elem) => elem.chemicalGroups !== null
-          )
-          approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
-            const checmialgroupArr = el.chemicalGroups
-              ?.toLowerCase()
-              .split(';')
-              .map((elem) => elem.trim())
-            return chemicalGroupSelected.every((chemgroup) =>
-              checmialgroupArr.find(
-                (elem) => elem === chemgroup.toLowerCase().trim()
-              )
-            )
-          })
-        }
-      } else {
-        approvedDisinfectantList = approvedDisinfectantList.filter(
-          (elem) => elem.chemicalGroups !== null
-        )
-        approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
-          const checmialgroupArr = el.chemicalGroups
-            ?.toLowerCase()
-            .split(';')
-            .map((elem) => elem.trim())
-          return checmialgroupArr.find(
-            (elem) => elem === chemicalGroupSelected.toLowerCase().trim()
-          )
-        })
-      }
-    }
+    approvedDisinfectantList = sortApprovedDisinfectantList(
+      approvedDisinfectantList
+    )
 
-    // approval categories filtration
-    const notApproved =
-      appSpecificConstants.diseaseOrdervaluesToOmit.notApproved
-    const approvalSuspended =
-      appSpecificConstants.diseaseOrdervaluesToOmit.approvalSuspended
+    approvedDisinfectantList = filterByChemicalGroup(
+      approvedDisinfectantList,
+      chemicalGroupSelected
+    )
 
-    const dynamicFilters = [
-      {
-        key: 'fmdo',
-        value: (item) =>
-          !item.fmdo?.includes(notApproved) &&
-          !item.fmdo?.includes(approvalSuspended)
-      },
-      {
-        key: 'svdo',
-        value: (item) =>
-          !item.svdo?.includes(notApproved) &&
-          !item.svdo?.includes(approvalSuspended)
-      },
-      {
-        key: 'dop',
-        value: (item) =>
-          !item.dop?.includes(notApproved) &&
-          !item.dop?.includes(approvalSuspended)
-      },
-      {
-        key: 'tbo',
-        value: (item) =>
-          !item.tbo?.includes(notApproved) &&
-          !item.tbo?.includes(approvalSuspended)
-      },
-      {
-        key: 'go',
-        value: (item) =>
-          !item.go?.includes(notApproved) &&
-          !item.go?.includes(approvalSuspended)
-      }
-    ]
+    approvedDisinfectantList = filterByApprovalCategories(
+      approvedDisinfectantList,
+      approvalCategoriesSelected
+    )
 
-    if (approvalCategoriesSelected) {
-      if (Array.isArray(approvalCategoriesSelected)) {
-        let dynamicFiltersSelected = dynamicFilters.filter((el) => {
-          return approvalCategoriesSelected?.includes(el.key)
-        })
+    approvedDisinfectantList = filterBySearchText(
+      approvedDisinfectantList,
+      searchText
+    )
 
-        dynamicFiltersSelected = dynamicFiltersSelected.map((elem) => {
-          return elem.value
-        })
+    approvedDisinfectantList = filterByStartsWith(
+      approvedDisinfectantList,
+      startsWith
+    )
 
-        if (approvalCategoriesSelected.length > 0) {
-          approvedDisinfectantList = approvedDisinfectantList.filter((item) =>
-            dynamicFiltersSelected.every((f) => f(item))
-          )
-        }
-      } else {
-        let dynamicFiltersSelected = dynamicFilters.filter((el) => {
-          return el.key === approvalCategoriesSelected
-        })
-
-        dynamicFiltersSelected = dynamicFiltersSelected.map((elem) => {
-          return elem.value
-        })
-
-        if (approvalCategoriesSelected.length > 0) {
-          approvedDisinfectantList = approvedDisinfectantList.filter((item) =>
-            dynamicFiltersSelected.every((f) => f(item))
-          )
-        }
-      }
-    }
-    // search text filtration
-    if (searchText) {
-      approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
-        return el.disInfectantName
-          ?.toLowerCase()
-          .includes(searchText?.toLowerCase())
-      })
-    }
-
-    // starts with filtration
-    if (startsWith) {
-      if (startsWith !== 'View all' && startsWith !== '0 to 9') {
-        approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
-          return el.disInfectantName
-            ?.toLowerCase()
-            .startsWith(startsWith.toLowerCase())
-        })
-      } else if (startsWith === '0 to 9') {
-        approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
-          return !isNaN(el.disInfectantName?.charAt(0))
-        })
-      }
-
-      logger.info(
-        `fetch-data executed ${JSON.stringify(approvedDisinfectantList)}`
-      )
-    }
+    logger.info(
+      `fetch-data executed ${JSON.stringify(approvedDisinfectantList)}`
+    )
   } catch (error) {
     logger.info(`error from fetch-data ${error.message}`)
     throw error
   }
+
   return {
     approvedDisinfectantList,
     checmicalGroups,
@@ -236,4 +152,179 @@ const fetchData = async (
     lastModifiedDate
   }
 }
+
+const getChemicalGroups = (getApprovedListResponse) => {
+  return getApprovedListResponse?.documents[0]?.chemicalGroups || []
+}
+
+const getLastModifiedTime = (getApprovedListResponse) => {
+  return getApprovedListResponse?.documents[0]?.lastModifiedDateAndTime
+}
+
+const getDisinfectantList = (getApprovedListResponse) => {
+  return getApprovedListResponse?.documents[0]?.disInfectants || []
+}
+
+const sortApprovedDisinfectantList = (approvedDisinfectantList) => {
+  return approvedDisinfectantList.sort(function (a, b) {
+    return a.disInfectantName.localeCompare(b.disInfectantName)
+  })
+}
+
+const filterByChemicalGroup = (
+  approvedDisinfectantList,
+  chemicalGroupSelected
+) => {
+  if (chemicalGroupSelected) {
+    if (Array.isArray(chemicalGroupSelected)) {
+      if (chemicalGroupSelected.length > 0) {
+        chemicalGroupSelected = chemicalGroupSelected.reduce(
+          (unique, item) =>
+            unique.includes(item) ? unique : [...unique, item],
+          []
+        )
+
+        approvedDisinfectantList = approvedDisinfectantList.filter(
+          (elem) => elem.chemicalGroups !== null
+        )
+
+        approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
+          const checmialgroupArr = el.chemicalGroups
+            ?.toLowerCase()
+            .split(';')
+            .map((elem) => elem.trim())
+
+          return chemicalGroupSelected.every((chemgroup) =>
+            checmialgroupArr.find(
+              (elem) => elem === chemgroup.toLowerCase().trim()
+            )
+          )
+        })
+      }
+    } else {
+      approvedDisinfectantList = approvedDisinfectantList.filter(
+        (elem) => elem.chemicalGroups !== null
+      )
+
+      approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
+        const checmialgroupArr = el.chemicalGroups
+          ?.toLowerCase()
+          .split(';')
+          .map((elem) => elem.trim())
+
+        return checmialgroupArr.find(
+          (elem) => elem === chemicalGroupSelected.toLowerCase().trim()
+        )
+      })
+    }
+  }
+
+  return approvedDisinfectantList
+}
+
+const filterByApprovalCategories = (
+  approvedDisinfectantList,
+  approvalCategoriesSelected
+) => {
+  const notApproved = appSpecificConstants.diseaseOrdervaluesToOmit.notApproved
+  const approvalSuspended =
+    appSpecificConstants.diseaseOrdervaluesToOmit.approvalSuspended
+
+  const dynamicFilters = [
+    {
+      key: 'fmdo',
+      value: (item) =>
+        !item.fmdo?.includes(notApproved) &&
+        !item.fmdo?.includes(approvalSuspended)
+    },
+    {
+      key: 'svdo',
+      value: (item) =>
+        !item.svdo?.includes(notApproved) &&
+        !item.svdo?.includes(approvalSuspended)
+    },
+    {
+      key: 'dop',
+      value: (item) =>
+        !item.dop?.includes(notApproved) &&
+        !item.dop?.includes(approvalSuspended)
+    },
+    {
+      key: 'tbo',
+      value: (item) =>
+        !item.tbo?.includes(notApproved) &&
+        !item.tbo?.includes(approvalSuspended)
+    },
+    {
+      key: 'go',
+      value: (item) =>
+        !item.go?.includes(notApproved) && !item.go?.includes(approvalSuspended)
+    }
+  ]
+
+  if (approvalCategoriesSelected) {
+    if (Array.isArray(approvalCategoriesSelected)) {
+      let dynamicFiltersSelected = dynamicFilters.filter((el) => {
+        return approvalCategoriesSelected?.includes(el.key)
+      })
+
+      dynamicFiltersSelected = dynamicFiltersSelected.map((elem) => {
+        return elem.value
+      })
+
+      if (approvalCategoriesSelected.length > 0) {
+        approvedDisinfectantList = approvedDisinfectantList.filter((item) =>
+          dynamicFiltersSelected.every((f) => f(item))
+        )
+      }
+    } else {
+      let dynamicFiltersSelected = dynamicFilters.filter((el) => {
+        return el.key === approvalCategoriesSelected
+      })
+
+      dynamicFiltersSelected = dynamicFiltersSelected.map((elem) => {
+        return elem.value
+      })
+
+      if (approvalCategoriesSelected.length > 0) {
+        approvedDisinfectantList = approvedDisinfectantList.filter((item) =>
+          dynamicFiltersSelected.every((f) => f(item))
+        )
+      }
+    }
+  }
+
+  return approvedDisinfectantList
+}
+
+const filterBySearchText = (approvedDisinfectantList, searchText) => {
+  if (searchText) {
+    approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
+      return el.disInfectantName
+        ?.toLowerCase()
+        .includes(searchText?.toLowerCase())
+    })
+  }
+
+  return approvedDisinfectantList
+}
+
+const filterByStartsWith = (approvedDisinfectantList, startsWith) => {
+  if (startsWith) {
+    if (startsWith !== 'View all' && startsWith !== '0 to 9') {
+      approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
+        return el.disInfectantName
+          ?.toLowerCase()
+          .startsWith(startsWith.toLowerCase())
+      })
+    } else if (startsWith === '0 to 9') {
+      approvedDisinfectantList = approvedDisinfectantList.filter((el) => {
+        return !isNaN(el.disInfectantName?.charAt(0))
+      })
+    }
+  }
+
+  return approvedDisinfectantList
+}
+
 export { fetchData }
