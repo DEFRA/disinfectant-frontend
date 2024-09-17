a3babc51db082d66f28d776ebcbff5a362ba3c64
abhishekdefra <abhishek.singha@defra.gov.uk>
2024-09-17 10:27:11 +0100

    changes in static text and page title

--- src/server/approved-disinfectants/staticTexts/pageSummaryTexts.js
+++ src/server/approved-disinfectants/staticTexts/pageSummaryTexts.js
@@ -1,8 +1,12 @@
 export const pageSummaryTexts = {
-  pageTitle: 'Approved Defra-approved disinfectants',
+  pageTitle: 'Defra-approved disinfectants',
   pageHeader: 'Disinfectants approved for use in England, Scotland and Wales',
   disinfectantTableHowToReadText:
     'The table below displays approved disinfectant products and their approved dilution rates for statutory use. Read the guidance on',
+  disinfectantTableHowToReadTextStart:
+    'The Disinfectants approval scheme is underpinned by the legislation',
+  disinfectantTableHowToReadTextStartLink:
+    'The Diseases of Animals (Approved Disinfectants) (England) Order 2007 (legislation.gov.uk)',
   disinfectantTableHowToReadLinkText:
     'To find out when you must use a Defra-approved disinfectant, which product to use, and in what concentration, visit',
   defraDisinfectantWhenAndHowText: 'Defra-approved disinfectants',
@@ -27,9 +31,9 @@ export const pageSummaryTexts = {
   searchText: 'Search',
   searchHintText: 'Type disinfectant name to search',
   viewFilterText: 'View filter options',
-  insetTextFirst: '* = Number of Millilitres To One Gram Disinfectant',
+  insetTextFirst: '* = Number of millilitres of water to one gram disinfectant',
   insetTextSecond:
-    'All other disinfectants are measured as Number of Parts Water To One Part Disinfectants.',
+    'All other approval rates are displayed as number of millilitres of water to be added to one part disinfectant, where one part disinfectant = 1 millilitre',
   disinfectantEnquiryText: 'Disinfectant enquiries',
   publishedText: 'Published',
   lastUpdatedText: 'Last Updated',
