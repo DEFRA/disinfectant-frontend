212efefb0ae2ce2c33de544e504780bd8ecef8a3
Loshanth Selvanayagam <loshanth.selavanayagam@defra.gov.uk>
2024-10-03 19:54:33 +0100

    removed the repetition of email

--- src/server/accessibility-statement/staticTexts/externalLink.js
+++ src/server/accessibility-statement/staticTexts/externalLink.js
@@ -1,4 +1,4 @@
 export const externalLink = {
   appliesToLink: 'https://disinfectants.defra.gov.uk/',
-  accessibilityMail: 'accessibility@defra.gov.uk'
+  accessibilityMail: 'accessibility@defra.gov.uk.'
 }
